2efd2dddacaae32e1b618f7008a7e30efe6221e8
vinigomes <vinigomes@outlook.com>
2017-07-04 22:28:52 -0700

    teste

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,10 @@ var Google = require('../src/Google');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+    console.log('here');
     var params = url.parse(req.url, true).query;
     var nomeDaEmpresa = params.nomeDaEmpresa;
+    console.log(nomeDaEmpresa);
     if (nomeDaEmpresa == '' || nomeDaEmpresa == undefined) {
         return res.render('index', { title: 'qcnpj-crawler' });
     }
@@ -15,6 +17,7 @@ router.get('/', function(req, res, next) {
     google.searchQcnpjLink(function(linkQcnpjDaEmpresa) {
         var qcnpj = new Qcnpj(linkQcnpjDaEmpresa);
         qcnpj.informacoesDaEmpresa(function(dadosDaEmpresa) {
+            console.log(dadosDaEmpresa);
             res.status(200).send(dadosDaEmpresa);
         });
     });
