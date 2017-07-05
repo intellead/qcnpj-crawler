164ed666f83eb33cf1055de363fb2bd3fd33e5ab
vinigomes <vinigomes@outlook.com>
2017-07-05 00:51:04 -0700

    fix

--- src/Company.js
+++ src/Company.js
@@ -7,12 +7,8 @@ class Company {
     constructor (name, cnpj, main_activity_text, main_activity_code, situation, social_capital, telephone) {
         this.company = name;
         this.cnpj = cnpj;
-        this.main_activity = [
-            {
-                main_activity_name: main_activity_text,
-                main_activity_code: main_activity_code
-            }
-        ];
+        this.main_activity_name = main_activity_text;
+        this.main_activity_code = main_activity_code
         this.company_situation = situation;
         this.company_social_capital = social_capital;
         this.company_telephone = telephone;
@@ -21,7 +17,7 @@ class Company {
     toString () {
         return  "Name: " + this.company + "\n" +
                 "CNPJ: " + this.cnpj + "\n" +
-                "Main Activity: " + this.main_activity + "\n" +
+                "Main Activity: " + this.main_activity_name + "\n" +
                 "Situation: " + this.company_situation + "\n" +
                 "Social Capital: " + this.company_social_capital + "\n" +
                 "Telephone: " + this.company_telephone + "\n";
