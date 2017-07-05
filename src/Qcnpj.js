9aa76eb4fc74abcf376298f399470a0594e878dc
vinigomes <vinigomes@outlook.com>
2017-07-04 23:56:40 -0700

    fix a bug

--- src/Qcnpj.js
+++ src/Qcnpj.js
@@ -16,14 +16,14 @@ class Qcnpj {
             }
             if(response.statusCode === 200) {
                 var $ = cheerio.load(body);
-                let company_name = $("li:contains('Razão Social')").children().main_activity_name();
-                let cnpj = $("li:contains('CNPJ')").children().main_activity_name();
-                let main_activity = $("li:contains('Atividade Principal')").children().main_activity_name();
+                let company_name = $("li:contains('Razão Social')").children().text();
+                let cnpj = $("li:contains('CNPJ')").children().text();
+                let main_activity = $("li:contains('Atividade Principal')").children().text();
                 let main_activity_code = main_activity.substr(0, main_activity.indexOf(' - '));
                 let main_activity_name = main_activity.substr((main_activity.indexOf(' - ') + 3), main_activity.length);
-                let situation = $("li:contains('Situação')").children().main_activity_name();
-                let social_capital = $("li:contains('Capital Social')").children().main_activity_name();
-                let telephone = $("li:contains('Telefone')").children().main_activity_name();
+                let situation = $("li:contains('Situação')").children().text();
+                let social_capital = $("li:contains('Capital Social')").children().text();
+                let telephone = $("li:contains('Telefone')").children().text();
                 var company = new Company(company_name, cnpj, main_activity_name, main_activity_code, situation, social_capital, telephone);
                 return callback(company);
             }
